7d204015a241334c0a51c73c4809dffd706b815f
nenadrad <nenad.rad@icloud.com>
2023-10-19 14:41:37 +0200

    Simplify Result pattern

--- RPSSL/Domain/Shared/Result.cs
+++ RPSSL/Domain/Shared/Result.cs
@@ -1,30 +1,18 @@
 namespace Domain.Shared;
 
-public class Result<T>
+public class Result
 {
     public bool IsSuccess { get; }
-    public T? Value { get; }
     public string? ErrorMessage { get; }
     
     public bool IsFailure => !IsSuccess;
 
-    protected Result(bool isSuccess, T? value = default, string? errorMessage = null)
+    private Result(bool isSuccess, string? errorMessage = null)
     {
         IsSuccess = isSuccess;
-        Value = value;
         ErrorMessage = errorMessage;
     }
 
-    public static Result<T> Success(T value) => new(true, value);
-    public static Result<T> Failure(string? errorMessage) => new(false, default, errorMessage);
-}
-
-public class Result : Result<object?>
-{
-    private Result(bool isSuccess, string? errorMessage = null) : base(isSuccess, null, errorMessage)
-    {
-    }
-
     public static Result Success() => new(true);
-    public new static Result Failure(string errorMessage) => new(false, errorMessage);
+    public static Result Failure(string errorMessage) => new(false, errorMessage);
 }
